f66fdc827095b1bfa142334d0a36bf65d0a77bdc
Anders Strömberg <coltlaboy@yahoo.se>
2025-10-28 10:45:59 +0000

    Add variable attribute to Google Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,14 @@ import { Geist, Geist_Mono } from "next/font/google";
 import type React from "react";
 import "../styles/globals.css";
 
-const geistSans = Geist({ subsets: ["latin"] });
-const geistMono = Geist_Mono({ subsets: ["latin"] });
+const geistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+});
 
 export const metadata: Metadata = {
   title: "v0 App",
